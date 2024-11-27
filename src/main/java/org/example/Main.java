7f5df4419ef6bc2794a31fea8b61dfc064acf762
Evgen <evgenygerasimoff@gmail.com>
2024-11-27 14:49:40 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> students = Arrays.asList(
+                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
+                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
+                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
+                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
+        );
+        // Используйте Parallel Stream для обработки данных и создания Map, где ключ - предмет, а значение - средняя оценка по всем студентам.
+        Map<String, Double> averageGradesBySubject = students.parallelStream()
+                .flatMap(student -> student.getGrades().entrySet().stream())
+                .collect(Collectors.groupingByConcurrent(
+                        subject -> subject.getKey(),
+                        Collectors.averagingInt(grade -> grade.getValue())
+                ));
+
+        // Выведите результат: общую Map с средними оценками по всем предмета
+        averageGradesBySubject.forEach((subject, avgGrade) ->
+                System.out.println("Subject: " + subject + ", Average grade: " + avgGrade)
+        );
+    }
+}
+
+class Student {
+    private String name;
+    private Map<String, Integer> grades;
+
+    public Student(String name, Map<String, Integer> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public Map<String, Integer> getGrades() {
+        return grades;
+    }
+}
